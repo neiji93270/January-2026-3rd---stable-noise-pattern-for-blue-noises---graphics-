9a1e7e3d47f31ca98dca013cd7fea1b916a10507
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2026-01-03 10:53:14 +0100

    Update VectorsPerPixrlLightingValuesScaleNoboundsOnFresnelAlgoIne.hlsl

--- VectorsPerPixrlLightingValuesScaleNoboundsOnFresnelAlgoIne.hlsl
+++ VectorsPerPixrlLightingValuesScaleNoboundsOnFresnelAlgoIne.hlsl
@@ -1,1 +1,17 @@
-//this would be good to orevent 
+//this would be good to orevent high scale noise on a previously used texture before zoom in.
+//A gaussian algorithm can overcome this.
+
+
+
+
+//The next step of the zoom in would be to zoom deeper with a correlated texture that simplify
+//the gaussian process above. so we can use holomorph functions to overcome the second zoom in on
+//correlated textures. The result would be to select a cleaarcoat texture that would be painted
+//with a close range of blue channels. So this is a loop. Then, can overcome this second issue with correlated
+//procedural textures. I mean we have both procedural correlated textures and painted textures.
+
+
+
+//To prevent having only a global view of the parameters sent to GPU, we can turn clusters to models.
+//The latent space could not be refactored here. So, once again, we need to select clearcoat textures.
+//This is here, only here, a computer vision process. So, the fresnel is usefull.
